05710cc794bbd26ebc70279ae717f3f9f6f38b6f
kolemasakar <kolemasakar@gmail.com>
2026-08-24 02:17:49 +0300

    M0 add storage layer baseline

---
+++ src/kgeopolitical_monitor/storage.py
@@ -0,0 +1,9 @@
+"""Storage layer baseline for K-Geopolitical Monitor.
+
+M0 implementation placeholder.
+Provides interfaces before concrete database integration.
+"""
+
+class Storage:
+    def initialize(self):
+        raise NotImplementedError
