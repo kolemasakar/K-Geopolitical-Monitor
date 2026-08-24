22d91c97e7fddbc5d5751fb249dfcea09ce2608a
kolemasakar <kolemasakar@gmail.com>
2026-08-24 07:27:24 +0300

    Add forecast metrics tests

---
+++ tests/test_forecast_metrics.py
@@ -0,0 +1,9 @@
+from kgeopolitical_monitor.forecast_metrics import calculate_brier_score, calculate_calibration_error
+
+
+def test_brier_score():
+    assert calculate_brier_score(0.8, 1.0) == 0.04
+
+
+def test_calibration_error():
+    assert calculate_calibration_error(0.7, 0.5) == 0.2
