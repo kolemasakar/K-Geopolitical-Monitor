a507d220ce1044a2d015bee59ed2da062d545544
kolemasakar <kolemasakar@gmail.com>
2026-08-26 11:48:59 +0300

    Restore repository interface contracts

--- src/kgeopolitical_monitor/repositories.py
+++ src/kgeopolitical_monitor/repositories.py
@@ -1,5 +1,22 @@
 """Repository interfaces for domain persistence."""
 
+
+class SourceRepository:
+    def save(self, source):
+        raise NotImplementedError
+
+    def get(self, source_id):
+        raise NotImplementedError
+
+
+class RawItemRepository:
+    def save(self, raw_item):
+        raise NotImplementedError
+
+    def get(self, raw_item_id):
+        raise NotImplementedError
+
+
 class EventRepository:
     def save(self, event):
         raise NotImplementedError
